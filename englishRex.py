20ffabb0261d1a2ae140673aa7ca8e07e01409d9
Daniel Breitlauch <github@flying-stampe.de>
2016-02-17 23:43:47 +0100

    support genre + ratings + clean up

--- englishRex.py
+++ englishRex.py
@@ -3,41 +3,49 @@ import re
 
 any = ' (an?y? )?'
 of = ' (of|from) '
+ofin = ' (of|from|in) '
 
 title = '(?P<title>.+)'
 menu = '(?P<menu>dashboard|home|menu|menü)'
 actor = '(?P<actor>.+)'
 season = '(?P<season>.+)'
 director = '(?P<director>.+)'
-rating = '(?P<rating>.*)'
-contentRating = '(?P<contentRating>.*)'
+higher_than_rating = '(?P<higher_rating>.+)'
+lower_than_rating = '(?P<lower_rating>.+)'
+contentRating = '(?P<contentRating>.+)'
+genreMovie = '(movies?|(?P<genre>.+?)( movies?)?) '
 
 navi = '(?P<navi>go( ?to)?)'
 play = '(?P<play>play)'
 cmd = '(' + navi + '|' + play + ')' + any
 
-newest = '(?P<newest>new(est)? )?'
-unseen = '(?P<unseen>unseen )?'
-newestUnseen = '(' + newest + '|' + unseen + ')'
+newest = '(?P<newest>(the )?new(est)?)?'
+unseen = '(?P<unseen>unseen)?'
+newestUnseen = '(' + newest + '|' + unseen + ') '
 
-year = '(year )?(?P<year>(19|20)(\d\d)?)'
+year = '(year )?(?P<year>(19|20)(\d\d)?)( |$)'
 decade = '(decade )?(?P<decade>(19|20)?\d\ds?)'
-time = '(' + year + '|' + decade + ')'
+time = '(the )?(' + year + '|' + decade + ')'
 
 regex = []
 regex.append((80, None, re.compile(cmd + menu, re.I)))    # go to dashboard
-regex.append((70, 'TV Shows', re.compile(cmd + 'episodes?' + of + title + of + 'season ' + season, re.I)))  # play (a) episode of <series> with <actor>
+regex.append((70, 'TV Shows', re.compile(cmd + newestUnseen + 'episodes?' + of + title + ofin + 'season ' + season, re.I)))  # play (a) episode of <series> with <actor>
+regex.append((70, 'TV Shows', re.compile(cmd + newestUnseen + 'episodes?' + ofin + 'season ' + season + of + title, re.I)))  # play (a) episode of <series> with <actor>
 regex.append((60, 'TV Shows', re.compile(cmd + newestUnseen + 'episodes?' + of + title, re.I)))  # play (a) new(est) or unseen episode of <series>
-regex.append((50, 'Movies', re.compile(cmd + newestUnseen + 'movie with' + any + actor, re.I)))    # play (newest, unseen) movie with <actor>
-regex.append((40, 'Movies', re.compile(cmd + newestUnseen + 'movie' + of + '(the )?(director )?' + director, re.I)))  # play (newest, unseen) movie from director <director>
-regex.append((30, 'Movies', re.compile(cmd + 'movies?' + of + 'the ' + time, re.I)))  # play a(ny) movie from the year or decade <year|decade>
-regex.append((20, 'Movies', re.compile(cmd + 'movies? with( a)?( better)? rating( above| better)? (as|than|then) ' + rating, re.I)))  # play a(ny) movie with a rating above <rating>
-regex.append((20, 'Movies', re.compile(cmd + 'movies? (with( a)? content rating( of)?| (which|who) are rated) ' + contentRating, re.I)))  # play a(ny) movie with a content rating  R
-regex.append((20, 'Movies', re.compile(cmd + contentRating + ' rated movies?', re.I)))  # play a(ny) R rated movie
+
+regex.append((50, 'Movies', re.compile(cmd + newestUnseen + genreMovie + 'with' + any + actor, re.I)))    # play (newest, unseen) movie with <actor>
+regex.append((40, 'Movies', re.compile(cmd + newestUnseen + genreMovie + of + '(the )?(director )?' + director, re.I)))  # play (newest, unseen) movie from director <director>
+regex.append((30, 'Movies', re.compile(cmd + newestUnseen + genreMovie + of + time, re.I)))  # play a(ny) movie from the year or decade <year|decade>
+regex.append((20, 'Movies', re.compile(cmd + newestUnseen + genreMovie + 'with( a)?( better| higher)? rating( above| better| higher| over)? (as|than|then) ' + higher_than_rating, re.I)))  # play a(ny) movie with a rating above <rating>
+regex.append((20, 'Movies', re.compile(cmd + newestUnseen + genreMovie + 'with( a)?( worse| lower)? rating( below| worse| lower| under)? (as|than|then) ' + lower_than_rating, re.I)))  # play a(ny) movie with a rating above <rating>
+regex.append((20, 'Movies', re.compile(cmd + newestUnseen + genreMovie + '(with( a)? content rating( of)?| (which|who) are rated) ' + contentRating, re.I)))  # play a(ny) movie with a content rating  R
+regex.append((20, 'Movies', re.compile(cmd + newestUnseen + genreMovie + 'rated ' + contentRating, re.I)))  # play a(ny) R rated movie
+regex.append((20, 'Movies', re.compile(cmd + newestUnseen + genreMovie + '$', re.I)))  # play a(ny) comedy (movie)
+regex.append((20, 'Movies', re.compile(cmd + contentRating + ' rated ' + newestUnseen + genreMovie, re.I)))  # play a(ny) R rated movie
 regex.append((10, None, re.compile(cmd + title, re.I)))   # play <title>
 
 
-# content rating R
+# combinations + refinement
 # non plex controls
 
 
--- plex.py
+++ plex.py
@@ -7,14 +7,14 @@ plex = PlexServer("http://192.168.0.30:32400")
 client = plex.client("Swordmaster")
 
 movies = plex.library.section('Movies')
-tv = plex.library.section('TV Shows')
+shows = plex.library.section('TV Shows')
 
 
-def flatMapTag(videoTag):
+def flatmap_tag(videoTag):
     if len(videoTag) == 0:
         return None
-    flat = ''
-    for t in videoTag:
+    flat = videoTag[0].id
+    for t in videoTag[1:]:
         flat += ',' + t.id
     videoTag[0].id = flat
     return videoTag[0]
@@ -39,74 +39,198 @@ def decade_fitt(name, decade):
     return name == decade
 
 
-def filter_person(plist, name):
-    return flatMapTag([a for a in plist if name_fitt(name, a.tag)])
+def case_insensitive_fitt(rating, serverRating):
+    return rating.lower() == serverRating.lower()
 
 
-def filter_decade(decadeslist, name):
-    d = [d for d in decadeslist if decade_fitt(name, d)]
-    if len(d) > 0:
-        return d[0]
-    return None
+def filter_person(person_list, name):
+    return flatmap_tag([a for a in person_list if name_fitt(name, a.tag)])
 
 
-def search(matched, found, priority):
-    title = matched.get('title')
-
-    newest = matched.get('newest')
-    unseen = matched.get('unseen')
-    year = matched.get('year')
-    rating = matched.get('rating')
-    season = matched.get('season')
-    contentRating = matched.get('contentRating')
+def filter_year(decades_list, name):
+    return flatmap_tag([a for a in decades_list if decade_fitt(name, a.tag)])
 
 
-    filter = 'all'
+def filter_case_insensitive(object_list, rating):
+    return flatmap_tag([a for a in object_list if case_insensitive_fitt(rating, a.tag)])
 
 
+def season_format(season):
+    if 'season ' not in season:
+        season = 'season ' + season
+    return season
+
+
+def create_filter(matched):
     args = dict()
     if 'actor' in matched:
-        res = filter_person(movies.actorObj(), matched['actor'])
+        res = filter_person(movies.get_actor(), matched['actor'])
         if res is None:
-            return
+            return None
         args['actor'] = res
 
     if 'director' in matched:
-        res = filter_person(movies.directorObj(), matched['director'])
+        res = filter_person(movies.get_director(), matched['director'])
         if res is None:
-            return
+            return None
         args['director'] = res
 
-    dec = None
-    if 'decade' in matched:
-        res = filter_decade(movies.decade(), matched['decade'])
+    if 'writer' in matched:
+        res = filter_person(movies.get_writer(), matched['writer'])
         if res is None:
-            return
-        dec = movies.decade(res)
+            return None
+        args['writer'] = res
 
-    if 'category' in matched:
-        section = plex.library.section(matched['category'])
-        search = section.search(title, filter='all', **args)
-        if dec:
-            search = [video for video in search if video in dec]
+    if 'producer' in matched:
+        res = filter_person(movies.get_producer(), matched['producer'])
+        if res is None:
+            return None
+        args['producer'] = res
 
-        for video in search:
-            found.append((priority, video))
+    if 'year' in matched:
+        res = filter_year(movies.get_year(), matched['year'])
+        if res is None:
+            return None
+        args['year'] = res
+
+    if 'decade' in matched:
+        res = filter_year(movies.get_decade(), matched['decade'])
+        if res is None:
+            return None
+        args['decade'] = res
+
+    if 'genre' in matched:
+        res = filter_case_insensitive(movies.get_genre(), matched['genre'])
+        if res is None:
+            return None
+        args['genre'] = res
+
+    if 'country' in matched:
+        res = filter_case_insensitive(movies.get_country(), matched['country'])
+        if res is None:
+            return None
+        args['country'] = res
+
+    if 'contentRating' in matched:
+        res = filter_case_insensitive(movies.get_content_rating(), matched['contentRating'])
+        if res is None:
+            return None
+        args['contentRating'] = res
+    return args
+
+
+def post_filter(matched, results):
+    if 'higher_rating' in matched and results:
+        border = float(matched['higher_rating'])
+        results = [video for video in results if hasattr(video, 'rating') and float(video.rating) > border]
+
+    if 'lower_rating' in matched and results:
+        border = float(matched['lower_rating'])
+        results = [video for video in results if hasattr(video, 'rating') and float(video.rating) < border]
+
+    if 'newest' in matched and len(results) > 1:
+        newest = results[0]
+        newest_date = results[0].originallyAvailableAt
+        for video in results:
+            if video.originallyAvailableAt > newest_date:
+                newest_date = video.originallyAvailableAt
+                newest = video
+        return [newest]
+
+    if 'oldest' in matched and len(results) > 1:
+        oldest = results[0]
+        oldest_date = results[0].originallyAvailableAt
+        for video in results:
+            if video.originallyAvailableAt < oldest_date:
+                oldest_date = video.originallyAvailableAt
+                oldest = video
+        return [oldest]
+    return results
+
+
+def search_movies(matched, found, priority):
+    title = matched.get('title')
+
+    if 'unseen' in matched:
+        movie_filter = 'unwatched'
     else:
-        section = plex
-        for video in section.search(title):
-            found.append(video)
+        movie_filter = 'all'
+
+    args = create_filter(matched)
+    if args is None:
+        return
+
+    results = movies.search(title, filter=movie_filter, **args)
+    results = post_filter(matched, results)
+    for video in results:
+        found.append((priority, video))
 
 
-# solution = match("go home")
+def search_episodes(matched, found, priority):
+    title = matched.get('title')
+    season = matched.get('season')
+
+    if 'unseen' in matched:
+        episode_filter = 'unwatched'
+        matched['oldest'] = 'added'
+    else:
+        episode_filter = 'all'
+
+    results = []
+    if title:
+        found_shows = shows.search(title, filter=episode_filter)
+        for show in found_shows:
+            if season:
+                season = show.season(season_format(season))
+                results = season.episodes()
+            else:
+                results = show.episodes(watched='unseen' not in matched)
+    else:
+        results = shows.searchEpisodes(None, filter=episode_filter)
+
+    results = post_filter(matched, results)
+    for video in results:
+        found.append((priority, video))
+
+
+def search_general(matched, found, priority):
+    title = matched.get('title')
+    results = plex.search(title)
+    results = post_filter(matched, results)
+    for video in results:
+        found.append((priority, video))
+
+
+# solutions = match("go home")
 # solutions = match("play episode of how i met your mother")
-solutions = match("go to any movie from the 60s")
+# solutions = match("go to any movie from the 60s")
+# solutions = match("go to any unseen movie from 2010")
+# solutions = match("go to any unseen movie rated PG-13")
+# solutions = match("go to any new movie with bruce willis")
+# solutions = match("go to any unseen movie with a rating higher than 8")
+# solutions = match("go to a fistful of datas")
+# solutions = match("go to any episode of how i met your mother from season 4")
+# solutions = match("go to the newest episode of how mother in season 3")
+# solutions = match("go to any unseen episode from limitless")
+# solutions = match("go to a new episode from agent carter")
+# solutions = match("go to an unseen romance")
+# solutions = match("go to an unseen action movie")
+# solutions = match("go to any new romance with george")
+# solutions = match("go to any unseen action movie rated PG-13")
+# solutions = match("go to any unseen action movie with a rating higher than 4")
+
 
 found = []
 threads = []
-for s, priority in solutions:
-    print(s)
-    thr = threading.Thread(target=search, args=[s, found, priority])
+for matched, priority in solutions:
+    print(matched)
+    if 'category' not in matched:
+        thr = threading.Thread(target=search_general, args=[matched, found, priority])
+    elif matched['category'] == 'TV Shows':
+        thr = threading.Thread(target=search_episodes, args=[matched, found, priority])
+    elif matched['category'] == 'Movies':
+        thr = threading.Thread(target=search_movies, args=[matched, found, priority])
+
     threads.append(thr)
     thr.start()
     thr.join()
@@ -120,8 +244,6 @@ print(found)
 
 # def search(self, title, filter='all', vtype=None, **tags):
 #        """ Search section content.
-#            title: Title to search (pass None to search all titles).
-#            filter: One of {'all', 'newest', 'onDeck', 'recentlyAdded', 'recentlyViewed', 'unwatched'}.
 #            videotype: One of {'movie', 'show', 'season', 'episode'}.
 #            tags: One of {country, director, genre, producer, actor, writer}.
 #        """
